7bcf04b7f3acfb210fbdd796bf3c6d58c9e84b06
[Josh Shepherd] <[optimizedcoder.py@gmail.com]>
2023-02-08 16:51:32 +0000

    about section add content and style

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,28 @@ import './About.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='about'>
+      <h2 className="about__heading">
+        About
+      </h2>
+      <p className="about__paragraph paragraph__one">
+        Welcome to the home of Mythos Rum, a premium 
+        rum brand inspired by the rich tapestry of
+        ancient myths and legends. Our mission is to
+        bring the spirit of myth to life with every
+        sip of our carefully crafted rum.
+      </p>
+      <p className="about__paragraph paragraph__two">
+        Our expert distillers blend the finest 
+        ingredients to create a smooth and complex 
+        flavour that evokes the power and <em>magic
+        </em> of myth. Whether you're a connoiseur or 
+        just looking to try something new, Mythos Rum
+        offers a truly unique and unforgettable drinking
+        experience. Join us on a journey to discover the
+        legends within
+      </p>
+    </section>
   )
 }
 
