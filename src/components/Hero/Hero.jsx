dacb4ca47d308e24df04b2758245630098eb93dc
[Josh Shepherd] <[optimizedcoder.py@gmail.com]>
2023-02-08 14:53:55 +0000

    header section and styling

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Hero.css'
+import logo from '../../assets/mythos_rum_logo.png'
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <article className="hero__left">
+        <div>
+        <h2>
+          Indulge in the mythic spirit of Mythos Rum
+        </h2>
+        <h3>
+        - Click to buy today!
+        </h3>
+        </div>
+        <div className="hero__left-ctas">
+        <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </article>
+      <article className="hero__right">
+      <img src={logo} alt="logo" className="hero__img" />
+      </article>
+    </section>
+  )
+}
+
+export default Hero
