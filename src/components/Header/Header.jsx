2890cd5cf4522bde133408ad5155a881fda35d2a
[Josh Shepherd] <[optimizedcoder.py@gmail.com]>
2023-02-09 13:17:01 +0000

    add mobile nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import './Header.css'
 import {AiOutlineShoppingCart} from 'react-icons/ai';
-import {MdOutlineStore} from 'react-icons/md'
-import {AiOutlineHome} from 'react-icons/ai'
+import {MdOutlineStore} from 'react-icons/md';
+import {AiOutlineHome} from 'react-icons/ai';
+import {BiMenuAltRight} from 'react-icons/bi';
 
 const Header = () => {
   return (
+    <div>
     <header>
       <section className="header__logo">
         <h1>Mythos Rum</h1>
@@ -28,7 +30,37 @@ const Header = () => {
           fontSize={26} />
         </a></li>
       </ul>
+      <div className="header__mobile-toggle">
+        {/* Mobile Nav */}
+        <BiMenuAltRight fontSize={26} 
+        // functionality
+        onClick={() => {
+          if (document.querySelector('.header__mobile-nav').style.display === 'none'){
+            document.querySelector('.header__mobile-nav').style.display = 'flex'
+          } else {
+            document.querySelector('.header__mobile-nav').style.display = 'none'
+          }
+        }} />
+      </div>
     </header>
+    <ul className="header__mobile-nav">
+        <li><a href="/">
+          <AiOutlineHome
+          color='#000'
+          fontSize={26} /> Home 
+        </a></li>
+        <li><a href="/">
+          <MdOutlineStore
+          color='#000'
+          fontSize={26} /> Store
+        </a></li>
+        <li><a href="/">
+          <AiOutlineShoppingCart
+          color='#000'
+          fontSize={26} /> Cart
+        </a></li>
+      </ul>
+      </div>
   )
 }
 
